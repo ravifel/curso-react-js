17b450cc5eb861133891b124261fe8de64df3b7d
Ravi Silva <ravifelipee@gmail.com>
2022-08-22 19:42:38 -0300

    otimizando a pagina de 'Home'

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,26 +1,6 @@
-import { Button } from "@mui/material";
 import React from "react";
-import { Link } from "react-router-dom";
 import Menu from "../../components/menu/Menu";
 
 export default function Home() {
-  return (
-    // <div style={{ marginLeft: "2em" }}>
-    //   <h1>Home</h1>
-    //   <Link to="/news" >
-    //     <Button to="/news" size="small" variant="contained">
-    //       Página News
-    //     </Button>
-    //   </Link>
-
-    //   <br />
-    //   <br />
-    //   <Link to="/counter" >
-    //     <Button to="/counter" size="small" variant="contained">
-    //       Página Counter
-    //     </Button>
-    //   </Link>
-    // </div>
-    <Menu/>
-  );
+  return <Menu />;
 }
